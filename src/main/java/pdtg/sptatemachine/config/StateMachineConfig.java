9b4c5b2df59a0164de5fee6aa5c1e97e7412fd76
Diego Tapia Guajardo <DiegoTGJobs@gmail.com>
2022-08-07 22:03:52 -0400

    Added State Machine Configs.

---
+++ src/main/java/pdtg/sptatemachine/config/StateMachineConfig.java
@@ -0,0 +1,30 @@
+package pdtg.sptatemachine.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.statemachine.config.EnableStateMachineFactory;
+import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
+import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
+import pdtg.sptatemachine.domain.PaymentEvent;
+import pdtg.sptatemachine.domain.PaymentState;
+
+import java.util.EnumSet;
+
+/**
+ * Created by Diego T. 07-08-2022
+ */
+@Slf4j
+@Configuration
+@EnableStateMachineFactory
+public class StateMachineConfig extends StateMachineConfigurerAdapter<PaymentState, PaymentEvent> {
+
+    @Override
+    public void configure(StateMachineStateConfigurer<PaymentState, PaymentEvent> states) throws Exception {
+        states.withStates()
+                .initial(PaymentState.NEW)
+                .states(EnumSet.allOf(PaymentState.class))
+                .end(PaymentState.AUTH)
+                .end(PaymentState.PRE_AUTH_ERROR)
+                .end(PaymentState.AUTH_ERROR);
+    }
+}

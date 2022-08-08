c3dc4d0e4e0715f8ad9bfa4d54fccf42ee8f2c25
Diego Tapia Guajardo <DiegoTGJobs@gmail.com>
2022-08-07 21:00:24 -0400

    Initial Commit

---
+++ src/main/java/pdtg/sptatemachine/SpTateMachineApplication.java
@@ -0,0 +1,13 @@
+package pdtg.sptatemachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpTateMachineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpTateMachineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pdtg/sptatemachine/SpTateMachineApplicationTests.java
@@ -0,0 +1,13 @@
+package pdtg.sptatemachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpTateMachineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
